56b898701a4757e1e67dd19ba465c3b29db4d9ec
fspjonny <fabiosilvapedro@yahoo.com.br>
2023-12-29 15:31:35 -0300

    dist files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/frontend_vue3_cadastro_de_profissionais/",
+  base: "/frontend_vue3_cadastro_de_profissionais/dist/",
   plugins: [vue()],
 })
