2046d8e5b8f0d69c913fdc9e00ff50aec2b1c462
fspjonny <fabiosilvapedro@yahoo.com.br>
2023-12-29 11:08:38 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/frontend_vue3_cadastro_de_profissionais/",
   plugins: [vue()],
 })
